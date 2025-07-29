2164206dc380fd2efed7c95adad30d08a6105126
Diiego-H <diegohernandezanton@gmail.com>
2025-07-29 11:25:28 +0000

    Fix ambiguous import

--- xrmocap/visualization/__init__.py
+++ xrmocap/visualization/__init__.py
@@ -1,8 +1,8 @@
 # yapf: disable
-from .visualize_keypoints2d import visualize_keypoints2d
-from xrprimer.visualization.keypoints.visualize_keypoints3d import visualize_keypoints3d
-from .visualize_keypoints3d import visualize_keypoints3d_projected
+from .visualize_keypoints2d import visualize_keypoints2d as visualize_kpts2d
+from xrprimer.visualization.keypoints.visualize_keypoints3d import visualize_keypoints3d as visualize_kpts3d
+from .visualize_keypoints3d import visualize_keypoints3d_projected as visualize_kpts3d_projected
 from .visualize_smpl import visualize_smpl_data
 
 # yapf: enable
-__all__ = ['visualize_keypoints2d', 'visualize_keypoints3d', 'visualize_keypoints3d_projected', 'visualize_smpl_data']
+__all__ = ['visualize_kpts2d', 'visualize_kpts3d', 'visualize_kpts3d_projected', 'visualize_smpl_data']
